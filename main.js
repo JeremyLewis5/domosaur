36fdae875eb5226da28c5bfb3ca2145569476ecd
JeremyLewis5 <jeremy.lewis@codeimmersives.com>
2021-03-16 10:54:17 -0500

    asdf

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ spanMess.style.fontSize = '40px';
 
 
 const paraMess = document.querySelector('p.mess-with-me');
-newSpan.style.backgroundColor = 'green';
+paraMess.style.backgroundColor = 'green';
 
 const dino1 = document.querySelector('#triceratops');
 dino1.style.width = '324px';
