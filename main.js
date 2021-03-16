61fca8afa8726d066230b2b6c2fcfeae50e7bdeb
JeremyLewis5 <jeremy.lewis@codeimmersives.com>
2021-03-16 10:32:10 -0500

    ads

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const newSpan = document.querySelector('.mess-with-me');
 newSpan.style.fontSize = '40px';
 newSpan.style.backgroundColor = 'green';
 
-const dino1 = document.querySelector('#"triceratops"');
+const dino1 = document.querySelector('#triceratops');
 dino1.style.width = '324px';
 
 
