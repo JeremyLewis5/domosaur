a40bf9405feb511c27584ce02bb5e7ab1da1f2cc
JeremyLewis5 <jeremy.lewis@codeimmersives.com>
2021-03-16 10:25:13 -0500

    asd

---
+++ main.js
@@ -0,0 +1,8 @@
+const newSpan = document.querySelector('.mess-with-me');
+newSpan.style.fontSize = '40px';
+newSpan.style.backgroundColor = 'green';
+
+const dino1 = document.querySelector('#"triceratops"');
+dino1.style.width = '324px';
+
+
